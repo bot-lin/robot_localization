e0980fe02aaaf3bb9e48d475880bc1332607a838
bot-lin <zlin6364@hotmail.com>
2023-02-24 12:44:59 +0800

    Update ekf.launch.py

--- launch/ekf.launch.py
+++ launch/ekf.launch.py
@@ -18,18 +18,25 @@ from ament_index_python.packages import get_package_share_directory
 import launch_ros.actions
 import os
 import yaml
-from launch.substitutions import EnvironmentVariable
+from launch.substitutions import EnvironmentVariable, LaunchConfiguration
 import pathlib
 import launch.actions
 from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
+    ekf_file = LaunchConfiguration('ekf_file')
+
+    declare_ekf_file_cmd = DeclareLaunchArgument(
+        'ekf_file',
+        default_value=os.path.join('/data', 'localization/ekf.yaml'),
+        description='Full path to the imu driver file')
     return LaunchDescription([
+        declare_ekf_file_cmd,
         launch_ros.actions.Node(
             package='robot_localization',
             executable='ekf_node',
             name='ekf_filter_node',
             output='screen',
-            parameters=[os.path.join(get_package_share_directory("robot_localization"), 'params', 'ekf.yaml')],
+            parameters=[ekf_file],
            ),
 ])
